9e929eb57bf1a8cb40fda16613cf2fd939761af0
takagi-135 <sa5ra12sa@gmail.com>
2024-10-09 14:45:33 +0900

    サンプル作成しました。

---
+++ src/app/components/footer/index.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div className="mx-0 mt-10 bg-[#5A3E19] w-full flex items-center justify-center p-2">
+        <p className="p-5 text-xl text-white">株式会社 印刷工房</p>
+      </div>
+    </footer>
+  );
+}
---
+++ src/app/components/header/index.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header>
+      <div className="m-0 w-full flex items-center justify-center p-3">
+        <p className="p-5 text-2xl font-semibold">レスポンシブ対応テーブル</p>
+      </div>
+    </header>
+  );
+}
